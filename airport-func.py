b3e512b103d62db923daff907577f569ebad6462
wtavi00 <avijittarafder22@gmail.com>
2025-01-26 15:09:21 +0600

    Update airport-func.py

--- airport-func.py
+++ airport-func.py
@@ -4,3 +4,17 @@ class Runway:
 
     def get_status(self):
         return self.status
+
+class Flight:
+    def __init__(self, fuel_status):
+        self.fuel_status = fuel_status
+
+    def display_status(self, runway_status):
+        if runway_status == "free":
+            print("Land")
+            self.display_landing_process()
+        elif self.fuel_status == "low":
+            print("Emergency landing")
+            self.display_landing_process()
+        else:
+            print("Circle in the air")
