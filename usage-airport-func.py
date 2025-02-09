d41835f76dc61738508a69823ca70041c5775eef
wtavi00 <avijittarafder22@gmail.com>
2025-02-09 16:07:50 +0600

    Update usage-airport-func.py

--- usage-airport-func.py
+++ usage-airport-func.py
@@ -3,3 +3,7 @@ runway = Runway(input("The status of the runway: "))
 fuel_status = input("If the fuel status is low, type 'low': ")
 flight = Flight(fuel_status)
 flight.display_status(runway.get_status())
+
+print("\n# Airline Check-In")
+airline_name = input("Input the name of the airlines (AirIndia, Emirates, British Airways): ")
+  
